ac6a9f1350b183f9ea89b63f2dc3d18730d744ce
TIZI <tenidiotzinternetudefault@gmail.com>
2024-08-07 18:19:54 +0200

    Created ISongModel and renamed the SongModel to SongFullModel.

---
+++ MyMusicTaste/Database/Models/ISongModel.cs
@@ -0,0 +1,6 @@
+namespace MyMusicTaste.Database.Models;
+
+public interface ISongModel
+{
+    public SongFullModel ToFullModel();
+}
---
+++ MyMusicTaste/Database/Models/SongFullModel.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace MyMusicTaste.Database.Models;
+
+public class SongFullModel : ISongModel
+{
+    private static readonly DateOnly InvalidDate = DateOnly.MaxValue;
+
+    public static IMongoCollection<SongFullModel> Collection = MongoDbConnection.Client
+        .GetDatabase("Core")
+        .GetCollection<SongFullModel>("Songs");
+    
+    [BsonElement("_id")]
+    [BindNever]
+    public string Id { get; set; }
+
+    public string? Title { get; set; }
+    public string? Artist { get; set; }
+
+    public string Album { get; set; } = "Unknown";
+
+    public string Genre { get; set; } = "Unknown";
+
+    public DateOnly ReleaseDate { get; set; }
+    
+    public string ReleaseDateString => ReleaseDate == InvalidDate ? 
+        "Unknown" : ReleaseDate.ToString();
+    
+    public SongFullModel ToFullModel() => this;
+}
