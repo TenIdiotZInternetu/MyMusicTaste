eef537817b7bbc6b7d1acb8dbcf3468662b06551
TIZI <tenidiotzinternetudefault@gmail.com>
2024-08-07 17:33:01 +0200

    Connected to Mongo via Stable API

--- MyMusicTaste/Database/Connections/MongoDbConnection.cs
+++ MyMusicTaste/Database/Connections/MongoDbConnection.cs
@@ -4,16 +4,19 @@ namespace MyMusicTaste.Database;
 
 public class MongoDbConnection
 {
+    private static readonly ServerApiVersion ApiVersion = ServerApiVersion.V1;
     public static MongoClient Client { get; private set; }
     
-    public static bool Connect(string? key)
+    public static void Connect(string? key)
     {
         if (string.IsNullOrEmpty(key))
         {
             throw new ArgumentException("A connection string is required to connect to the database.");
         }
+
+        var mongoSettings = MongoClientSettings.FromConnectionString(key);
+        mongoSettings.ServerApi = new ServerApi(ApiVersion);
         
-        Client = new MongoClient(key);
-        return true;
+        Client = new MongoClient(mongoSettings);
     }
 }
