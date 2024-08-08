ef955c5e2ae0a364125ce03a2aefa30636ddddc2
TIZI <tenidiotzinternetudefault@gmail.com>
2024-08-08 18:11:37 +0200

    Created EntryNotFoundException

--- MyMusicTaste/Database/Operations/DatabaseOperationException.cs
+++ MyMusicTaste/Database/Operations/DatabaseOperationException.cs
@@ -3,3 +3,5 @@ namespace MyMusicTaste.Database.Operations;
 public class DatabaseOperationException(string message) : Exception(message);
 
 public class EntryAlreadyExistsException(string message) : DatabaseOperationException(message);
+
+public class EntryNotFoundException(string message) : DatabaseOperationException(message);
