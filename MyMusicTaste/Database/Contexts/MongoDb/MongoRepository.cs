060dcfdb60a01a3d030175701492026945853f68
TIZI <tenidiotzinternetudefault@gmail.com>
2024-08-09 22:36:21 +0200

    Moved GetModelById implementation from GenericDbOperations to MongoRepository.cs

--- MyMusicTaste/Database/Contexts/MongoDb/MongoRepository.cs
+++ MyMusicTaste/Database/Contexts/MongoDb/MongoRepository.cs
@@ -1,10 +1,46 @@
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MyMusicTaste.Database.Connections;
+using MyMusicTaste.Database.Operations;
+
 namespace MyMusicTaste.Database.Contexts.MongoDb;
 
 public class MongoRepository<TModel> : IDbRepository<TModel>
 {
+    public IMongoCollection<TModel> Collection { get; init; }
+    public IMongoDatabase Database => Collection.Database;
+
+    public MongoRepository(string databaseName, string collectionName)
+    {
+        var client = MongoDbContext.Client;
+        Collection = client.GetDatabase(databaseName).GetCollection<TModel>(collectionName);
+    }
+    
     public TModel GetById(string? id)
     {
-        throw new NotImplementedException();
+        bool idIsValid = ObjectId.TryParse(id, out ObjectId objectId);
+        
+        if (!idIsValid)
+        {
+            throw new EntryNotFoundException("Invalid ID!");
+        }
+
+        return GetById(objectId);
+    }
+    
+    public TModel GetById(ObjectId id)
+    {
+        var filter = Builders<TModel>.Filter
+            .Eq(x => x.Id, id);
+
+        TModel entry = Collection.Find(filter).FirstOrDefault();
+        
+        if (entry == null)
+        {
+            throw new EntryNotFoundException("Entry not found!");
+        }
+
+        return entry;
     }
 
     public Task<TModel> GetByIdAsync(string? id)
