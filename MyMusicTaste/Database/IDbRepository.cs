081bc38e7d5476efdecaa093b92f92c5f3199a50
TIZI <tenidiotzinternetudefault@gmail.com>
2024-08-09 22:25:20 +0200

    Added async versions of IDbRepository methods.

--- MyMusicTaste/Database/IDbRepository.cs
+++ MyMusicTaste/Database/IDbRepository.cs
@@ -3,7 +3,11 @@ namespace MyMusicTaste.Database;
 public interface IDbRepository<TModel>
 {
     public TModel GetById(string? id);
+    public Task<TModel> GetByIdAsync(string? id);
     public void Create(TModel model);
+    public Task CreateAsync(TModel model);
     public void Update(TModel model);
+    public Task UpdateAsync(TModel model);
     public void Delete(TModel model);
+    public Task DeleteAsync(TModel model);
 }
