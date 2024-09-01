c4b64685d9bfa73e0f17983108b547d3c30cdc32
TIZI <tenidiotzinternetudefault@gmail.com>
2024-09-01 21:36:46 +0200

    Stylised single Song search item

--- MyMusicTaste/Components/SearchComps/SongSearchItem.razor.cs
+++ MyMusicTaste/Components/SearchComps/SongSearchItem.razor.cs
@@ -1,7 +1,12 @@
 using Microsoft.AspNetCore.Components;
+using MyMusicTaste.Models;
 
 namespace MyMusicTaste.Components.SearchComps;
 
 public partial class SongSearchItem : ComponentBase
 {
+    [Parameter]
+    public Song? Song {get; set;}
+    
+    private bool _hasCover => Song.CoverImageLink != null;
 }
