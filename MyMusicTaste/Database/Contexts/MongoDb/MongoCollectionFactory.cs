ff163b4e7ce50bfb89e515fcc2b806205c22e6c6
TIZI <tenidiotzinternetudefault@gmail.com>
2024-09-02 20:16:24 +0200

    Created and implemented MongoCollectionFactory.cs

---
+++ MyMusicTaste/Database/Contexts/MongoDb/MongoCollectionFactory.cs
@@ -0,0 +1,23 @@
+using MongoDB.Driver;
+using MyMusicTaste.Database.Connections;
+using MyMusicTaste.Models;
+
+namespace MyMusicTaste.Database.Contexts.MongoDb;
+
+public class MongoCollectionFactory
+{
+    private record struct CollectionInfo(string DbName, string CollectionName);
+
+    private static readonly Dictionary<Type, CollectionInfo> _collectionMapping = new()
+    {
+        { typeof(User), new CollectionInfo("Core", "Users") },
+        { typeof(Song), new CollectionInfo("Core", "Songs") }
+    };
+    
+    public static IMongoCollection<TModel> Create<TModel>() where TModel : Model
+    {
+        var client = MongoDbContext.Client;
+        var collInfo = _collectionMapping[typeof(TModel)];
+        return client.GetDatabase(collInfo.DbName).GetCollection<TModel>(collInfo.CollectionName);
+    }
+}
