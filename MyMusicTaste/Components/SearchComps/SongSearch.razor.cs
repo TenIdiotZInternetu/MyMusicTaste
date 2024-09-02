775172af4d2e57215e0b5cf98b1c82b977c24684
TIZI <tenidiotzinternetudefault@gmail.com>
2024-09-02 23:34:17 +0200

    Implemented SongSearch. i think

--- MyMusicTaste/Components/SearchComps/SongSearch.razor.cs
+++ MyMusicTaste/Components/SearchComps/SongSearch.razor.cs
@@ -1,7 +1,18 @@
 using Microsoft.AspNetCore.Components;
+using MongoDB.Driver.Search;
+using MyMusicTaste.Models;
 
 namespace MyMusicTaste.Components.SearchComps;
 
 public partial class SongSearch : ComponentBase
 {
+    [Parameter] 
+    public int ResultsCount { get; set; }
+    
+    private List<Song> _results { get; set; }
+
+    private void SearchSongs(string query)
+    {
+        _results = Searcher.Search(query, ResultsCount);
+    }
 }
