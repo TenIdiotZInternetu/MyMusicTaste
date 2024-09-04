d7832722a2dcb136091a9088f2122d1b384d177e
TIZI <tenidiotzinternetudefault@gmail.com>
2024-09-05 00:53:13 +0200

    Implemented GetRatingsByUser method of MongoSongRatingListing

--- MyMusicTaste/Database/Contexts/MongoDb/Operations/MongoSongRatingListing.cs
+++ MyMusicTaste/Database/Contexts/MongoDb/Operations/MongoSongRatingListing.cs
@@ -6,25 +6,40 @@ using MyMusicTaste.Models;
 
 namespace MyMusicTaste.Database.Contexts.MongoDb.Operations;
 
-public class SongRatingListing : ISongRatingListing
+public class MongoSongRatingListing : ISongRatingListing
 {
-    // Collection acquirement is subject to change
-    private IMongoCollection<RatingDto> _collection = MongoDbContext.Client
-        .GetDatabase("Core").GetCollection<RatingDto>("SongRatings");
-        
     private class RatingDto : SongRating
     {
         public ObjectId UserId;
         public ObjectId SongId;
     }
     
+    // Collection acquirement is subject to change
+    private IMongoCollection<RatingDto> _collection = MongoDbContext.Client
+        .GetDatabase("Core").GetCollection<RatingDto>("SongRatings");
+
+    private IDbRepository<User> _usersRepo;
+    private IDbRepository<Song> _songsRepo;
+
+    public MongoSongRatingListing(IDbRepository<User> usersRepo, IDbRepository<Song> songsRepo)
+    {
+        _usersRepo = usersRepo;
+        _songsRepo = songsRepo;
+    }
+
     public IEnumerable<SongRating> GetRatingsByUser(User user)
     {
         var filter = Builders<RatingDto>.Filter
             .Eq(x => x.UserId, user.Id);
 
         List<RatingDto> results = _collection.Find(filter).ToList();
-        return results;
+        return results.ToList().Select(dto => new SongRating()
+        {
+            Id = dto.Id,
+            User = user,
+            Song = _songsRepo.GetById(dto.SongId),
+            Rating = dto.Rating
+        });
     }
 
     public IEnumerable<SongRating> GetRaginsBySong(Song song)
