a76f528713146ff2fb68eddb730fe076105c5a8d
TIZI <tenidiotzinternetudefault@gmail.com>
2024-09-05 00:41:33 +0200

    Implemented GetRatingsByUser

--- MyMusicTaste/Database/Contexts/MongoDb/Operations/MongoSongRatingListing.cs
+++ MyMusicTaste/Database/Contexts/MongoDb/Operations/MongoSongRatingListing.cs
@@ -1,3 +1,6 @@
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MyMusicTaste.Database.Connections;
 using MyMusicTaste.Database.Operations;
 using MyMusicTaste.Models;
 
@@ -5,9 +8,23 @@ namespace MyMusicTaste.Database.Contexts.MongoDb.Operations;
 
 public class SongRatingListing : ISongRatingListing
 {
+    // Collection acquirement is subject to change
+    private IMongoCollection<RatingDto> _collection = MongoDbContext.Client
+        .GetDatabase("Core").GetCollection<RatingDto>("SongRatings");
+        
+    private class RatingDto : SongRating
+    {
+        public ObjectId UserId;
+        public ObjectId SongId;
+    }
+    
     public IEnumerable<SongRating> GetRatingsByUser(User user)
     {
-        throw new NotImplementedException();
+        var filter = Builders<RatingDto>.Filter
+            .Eq(x => x.UserId, user.Id);
+
+        List<RatingDto> results = _collection.Find(filter).ToList();
+        return results;
     }
 
     public IEnumerable<SongRating> GetRaginsBySong(Song song)
