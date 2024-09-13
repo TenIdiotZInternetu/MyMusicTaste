f91bd911632153063c2c36808cb738538d6aaffa
TIZI <tenidiotzinternetudefault@gmail.com>
2024-09-13 16:20:10 +0200

    Refactored SongSearch component into more generic SearchComponent, and made SongSearch inherit it.

--- MyMusicTaste/Components/SearchComps/SearchComponent.cs
+++ MyMusicTaste/Components/SearchComps/SearchComponent.cs
@@ -1,28 +1,33 @@
 using Microsoft.AspNetCore.Components;
 using MongoDB.Driver.Search;
+using MyMusicTaste.Database.Operations;
 using MyMusicTaste.Models;
 
 namespace MyMusicTaste.Components.SearchComps;
 
-public partial class SongSearch : ComponentBase
+public class SearchComponent<TModel> : ComponentBase
+    where TModel : Model
 {
     [Parameter]
     public string? Query { get; set; }
     
     [Parameter] 
     public int ResultsCount { get; set; }
+
+    [Inject] 
+    protected ISearchOperation<TModel>? Searcher { get; set; }
     
-    private List<Song>? _results { get; set; }
+    protected List<TModel>? Results { get; set; }
     
     public void UpdateResults()
     {
         if (Query == null)
         {
-            _results = null;
+            Results = null;
             return;
         }
         
-        _results = Searcher.Search(Query, ResultsCount);
+        Results = Searcher.Search(Query, ResultsCount);
         StateHasChanged();
     }
 
