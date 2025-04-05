479c6d0a6475ec445fc48a876bb18b35c1b3ea52
TIZI <tenidiotzinternetudefault@gmail.com>
2025-04-05 16:53:04 +0200

    Refactored Login to use MVC

---
+++ MyMusicTaste/Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using MyMusicTaste.Components.Pages;
+using MyMusicTaste.Database;
+
+namespace MyMusicTaste.Controllers;
+
+[Route("auth")]
+public class AuthController : Controller {
+    public const string LOGIN_URI = "auth/login";
+    public const string LOGOUT_URI = "auth/logout";
+    
+    public class LoginDto : IUserLoginDto
+    {
+        [Required(ErrorMessage = "Enter your username.")]
+        [StringLength(24)]
+        public string? Username { get; set; }
+        
+        [Required(ErrorMessage = "Enter your password.")]
+        [MinLength(8)]
+        [MaxLength(32)]
+        public string? Password { get; set; }
+        
+        public string? ReturnUrl { get; set; }
+    }
+    
+    private readonly IIdentityProvider _identity;
+
+    public AuthController(IIdentityProvider identity) {
+        _identity = identity;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> LoginAsync([FromForm] LoginDto loginDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest();
+        }
+        
+        var result = await _identity.LoginUserAsync(loginDto);
+
+        if (result.Succeeded)
+        {
+            return Redirect(HomePage.GetRoute());
+        }
+        
+        return Unauthorized();
+    }
+    
+    
+    [HttpPost("logout")]
+    public async Task<IActionResult> LogoutAsync() {
+        await _identity.LogOutUserAsync();
+        return Redirect("/");
+    }
+}
