9e5a84623dc536166f724be74eb41344b43cc820
touster <tenidiotzinternetudefault@gmail.com>
2025-07-20 20:47:40 +0200

    Added SourceLink with Validation to Song

--- MyMusicTaste/Models/Song/Song.cs
+++ MyMusicTaste/Models/Song/Song.cs
@@ -4,15 +4,37 @@ public class Song : Model
 {
     private static readonly DateOnly INVALID_DATE = DateOnly.MaxValue;
 
-    public string? Title { get; set; }
-    public string? Author { get; set; }
-
-    public string Album { get; set; } = "Unknown";
-    public string Genre { get; set; } = "Unknown";
+    public string Title { get; set; } = null!;
+    public string Author { get; set; } = null!;
+    public string? Album { get; set; }
+    public string? Genre { get; set; }
+    
     public DateOnly ReleaseDate { get; set; } = INVALID_DATE;
+    public bool ReleasDateValid => ReleaseDate != INVALID_DATE;
+    public string? ReleaseDateString => ReleasDateValid ? 
+        null : ReleaseDate.ToString();
     
     public string? CoverImageLink { get; set; }
-    
-    public string ReleaseDateString => ReleaseDate == INVALID_DATE ? 
-        "Unknown" : ReleaseDate.ToString();
+    public string? SourceLink { get; private set; }
+
+    public bool ValidateSourceLink(string link)
+    {
+        string[] validDomains =
+        {
+            "youtube.com/watch", 
+            "music.youtube.com",
+            "youtu.be/",
+            "open.spotify.com/track",
+            "soundcloud.com/",
+            "music.apple.com/"
+        };
+
+        if (!validDomains.Any(link.Contains))
+        {
+            return false;
+        }
+        
+        SourceLink = link;
+        return true;
+    }
 }
